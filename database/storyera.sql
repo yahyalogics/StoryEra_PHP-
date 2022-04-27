37c194c8e2dc90a0a6179eccfad4aca1a7a62806
Furqan Rasool <100808717+yahyalogics@users.noreply.github.com>
2022-04-27 23:12:16 +0500

    Add files via upload

---
+++ database/storyera.sql
@@ -0,0 +1,25 @@
+create database storyera
+use storyera
+
+
+CREATE TABLE account (
+id INT PRIMARY KEY,
+username VARCHAR(30) NOT NULL,
+password VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+)
+
+CREATE TABLE stories (
+title VARCHAR(30) NOT NULL,
+name VARCHAR(30) NOT NULL,
+description VARCHAR(700),
+location VARCHAR(20),
+category VARCHAR(20),
+image VARCHAR(20),
+)
+
+CREATE TABLE Admin (
+username VARCHAR(30) NOT NULL,
+password VARCHAR(30) NOT NULL,
+
+)

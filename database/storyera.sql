ad972999cee4e6363aa2774e16a14390895f0d2a
Furqan Rasool <100808717+yahyalogics@users.noreply.github.com>
2022-04-28 16:42:25 +0500

    Update storyera.sql

--- database/storyera.sql
+++ database/storyera.sql
@@ -1,13 +1,16 @@
-create database storyera
-use storyera
+select * from stories;
+select * from account;
 
+INSERT INTO stories (title, name, description, location, category)
+VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger' , 'Stavanger');
 
+use storyera;
 CREATE TABLE account (
 id INT PRIMARY KEY,
 username VARCHAR(30) NOT NULL,
 password VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-)
+email VARCHAR(50)
+);
 
 CREATE TABLE stories (
 title VARCHAR(30) NOT NULL,
@@ -15,11 +18,10 @@ name VARCHAR(30) NOT NULL,
 description VARCHAR(700),
 location VARCHAR(20),
 category VARCHAR(20),
-image VARCHAR(20),
-)
+image VARCHAR(20)
+);
 
 CREATE TABLE Admin (
 username VARCHAR(30) NOT NULL,
-password VARCHAR(30) NOT NULL,
-
-)
+password VARCHAR(30) NOT NULL
+);
